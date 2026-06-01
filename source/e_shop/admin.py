25678b849d6ccc439265c8ff5ec75c79a008a53c
iAmBelovNikita <iambelovnikita@gmail.com>
2026-06-01 16:30:18 +0600

    Add admin code for manipulate models

--- source/e_shop/admin.py
+++ source/e_shop/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from .models import Category, Product
+
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description')
+    list_filter = ('title',)
+    fields = ('title', 'description')
+    search_fields = ('title', 'description')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'price', 'created_at', 'image_link')
+    list_filter = ('category', 'created_at', 'price')
+    fields = ('title', 'description', 'category', 'price', 'image_link', 'created_at')
+    search_fields = ('title', 'description', 'category__title', 'price')
+    readonly_fields = ('created_at',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
